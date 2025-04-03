4d4d06e3aed34bd1b94d30e40dc2cc39afef7e2f
Barreto <phenriquevb@hotmail.com>
2025-04-02 23:46:33 -0300

    first commit

---
+++ usuarios/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://emporiomodelo:emporiomodelo2025@localhost/db_usuarios"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
