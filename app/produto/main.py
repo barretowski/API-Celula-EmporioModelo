3feed9e58a854b80630c0a4951f1f00eaef6eafc
Barreto <phenriquevb@hotmail.com>
2025-04-22 21:01:05 -0300

    change: ajustes docker

--- app/produto/main.py
+++ app/produto/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routes.routeProduto import router as produto_router
 from routes.routeCategoria import router as categoria_router
+from database import engine, Base
 
 app = FastAPI(
     title="Célula Produto",
     description="Gerenciamento de Produtos e Categorias",
     version="1.0.0"
 )
+Base.metadata.create_all(bind=engine)
 app.include_router(produto_router, prefix="/produto", tags=["Produto"])
 app.include_router(categoria_router, prefix="/categoria", tags=["Categoria"])
--- app/produto/schemas/schemaCategoria.py
+++ app/produto/schemas/schemaCategoria.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator, EmailStr
+from pydantic import BaseModel
 
 class CategoriaBase(BaseModel):
     descricao: str
